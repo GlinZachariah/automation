20fc0babeaefc69dd53350ae5f2a521a9e8087e0
GlinZachariah <glincool.zac@gmail.com>
2019-12-28 01:05:46 +0530

    configured files and added database style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { JobsComponent } from './jobs/jobs.component';
+import { DatabasesComponent } from './databases/databases.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"jobs",component:JobsComponent},
+  {path:"databases",component:DatabasesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule,ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { DatabasesComponent } from './databases/databases.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobsComponent,
+    DatabasesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
